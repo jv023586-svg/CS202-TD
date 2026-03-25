c838cc79a7107c1792cdfd7b3ffa8b47ea4a1419
Gerald Valentin <jv023586@gmail.com>
2026-03-25 14:50:09 -0400

    Initial SFML setup with CMake and working window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
-// Basic hello world skeleton
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello\n";
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML Works");
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
