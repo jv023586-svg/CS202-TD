10ba7137155cf17f0d4493ac23aecd73a76a7caa
Eliana Nichols <eliananichols@cheetah-mcdfk.device.utk.edu>
2026-04-08 11:44:46 -0400

    Added Projectiles.h and Projectiles.cpp. Filled out the .h file and started working on the skeleton for the .cpp

---
+++ src/Projectiles.cpp
@@ -0,0 +1,30 @@
+#include "Projectiles.h"
+
+/*
+    // variables
+    sf::Vector2f position;
+    sf::Vector2f velocity;
+    float damage;
+    bool isActive;
+    Enemy* target;
+*/
+Projectile::Projectile()
+{
+
+}
+
+void update(float dt)
+{
+
+}
+
+void draw(window)
+{
+
+}
+
+bool isAlive()
+{
+
+}
+
