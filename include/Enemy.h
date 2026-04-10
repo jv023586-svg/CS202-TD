e3774812e120f2dabfb1716c6ce4a4ae5d236b64
Connor Cooper <ccoope64@vols.utk.edu>
2026-04-10 11:09:33 -0400

    Filled out enemy.h

--- include/Enemy.h
+++ include/Enemy.h
@@ -7,4 +7,19 @@ public:
 
     void update();
     void draw(sf::RenderWindow& window);
+
+    void takeDamage(int amount);
+    bool isAlive() const;
+    void attack();
+
+private:
+    float health;
+    float maxHealth;
+    float damage;
+    float attackRate;
+    float attackCooldown;
+    float speed;
+
+    sf::Vector2f position;
+    sf::Vector2f target;
 };
