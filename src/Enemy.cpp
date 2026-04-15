9b507cf21c966d11def1e7589c9b1eca3bcbb8af
Connor Cooper <ccoope64@vols.utk.edu>
2026-04-15 10:56:27 -0400

    Fix implementation

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,29 +1,35 @@
 #include "Enemy.h"
+#include <iostream>
 
-using namespace std;
-
-Enemy::Enemy(float health(100.0f), float maxHealth(100.0f), float damage(10.0f), float attackRate(1.0f), 
-             float attackCooldown(1.0f), float speed(1.0f), const sf::Vector2f& position(0.0f, 0.0f), 
-             const sf::Vector2f& target(0.0f, 0.0f)) 
+Enemy::Enemy()
+    : health(100.0f),
+      maxHealth(100.0f),
+      damage(10.0f),
+      attackRate(1.0f),
+      attackCooldown(1.0f),
+      speed(1.0f),
+      position(0.0f, 0.0f),
+      target(0.0f, 0.0f)
 {
-
     if (!texture.loadFromFile("assets/textures/bloop.png"))
     {
-        std::cerr << "Failed to load enemy texture assets/textures/bloop.png" 
-                  << std::endl;
+        std::cerr << "Failed to load enemy texture assets/textures/bloop.png\n";
     }
 
+    // Sets scale, texture, start position of enemy
     sprite.setTexture(texture);
     sprite.setPosition(position);
-    sprite.setScale(0.5f, 0.5f);    
+    sprite.setScale({0.5f, 0.5f});
 }
 
-void Enemy::update(float dt) 
+void Enemy::update(float dt)
 {
-    
+    // temporary test movement
+    position.x += speed * dt * 60.0f;
+    sprite.setPosition(position);
 }
 
-void Enemy::draw(sf::RenderWindow& window) 
+void Enemy::draw(sf::RenderWindow& window)
 {
     window.draw(sprite);
 }
@@ -36,7 +42,7 @@ void Enemy::takeDamage(int amount)
         health = 0;
     }
 }
-    
+
 bool Enemy::isAlive() const
 {
     return health > 0;
@@ -44,5 +50,4 @@ bool Enemy::isAlive() const
 
 void Enemy::attack()
 {
-
 }
