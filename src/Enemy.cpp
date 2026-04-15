7ab9771c9afa1efdae1549dff642c4b48ea72361
Connor Cooper <ccoope64@vols.utk.edu>
2026-04-15 10:16:30 -0400

    Filled out constructor

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,11 +1,48 @@
 #include "Enemy.h"
-#include <iostream>
 
-Enemy::Enemy() {
+using namespace std;
+
+Enemy::Enemy(float health(100.0f), float maxHealth(100.0f), float damage(10.0f), float attackRate(1.0f), 
+             float attackCooldown(1.0f), float speed(1.0f), const sf::Vector2f& position(0.0f, 0.0f), 
+             const sf::Vector2f& target(0.0f, 0.0f)) 
+{
+
+    if (!texture.loadFromFile("assets/textures/bloop.png"))
+    {
+        std::cerr << "Failed to load enemy texture assets/textures/bloop.png" 
+                  << std::endl;
+    }
+
+    sprite.setTexture(texture);
+    sprite.setPosition(position);
+    sprite.setScale(0.5f, 0.5f);    
 }
 
-void Enemy::update() {
+void Enemy::update(float dt) 
+{
+    
 }
 
-void Enemy::draw(sf::RenderWindow& window) {
+void Enemy::draw(sf::RenderWindow& window) 
+{
+    window.draw(sprite);
+}
+
+void Enemy::takeDamage(int amount)
+{
+    health -= amount;
+    if (health <= 0)
+    {
+        health = 0;
+    }
+}
+    
+bool Enemy::isAlive() const
+{
+    return health > 0;
+}
+
+void Enemy::attack()
+{
+
 }
