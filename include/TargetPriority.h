b6434e6f5d633a1c35a4fcfb496fd5cb7d0b0211
Jerry <jv023586@gmail.com>
2026-04-22 15:02:45 -0400

    Update TargetPriority.h

--- include/TargetPriority.h
+++ include/TargetPriority.h
@@ -1,7 +1,89 @@
-#pragma once
+/*
+add()
+  search
+remove()
+Node constructor()
+  next
+  prev
+  head
+  tail
+  data
+rangecheck()
+*/
 
-/// How Capsule (and Turret, via shared target) picks among enemies in range.
-enum class TargetPriority {
-    Random, ///< Pick uniformly among valid targets (chaotic demo).
-    First   ///< Lowest spawn index first (ordered demo; TargetQueue can back this later).
-};
+const float capRange = capsule.getAttackRange();
+const float capRangeSq = capRange * capRange;
+
+//It should take as arguments targetPriority and should return a pointer to an enemy.
+//If targetPriority == Oldest, return the enemy that HEAD points to
+//If targetPriority == Newest, return the enemy that tail points to
+Enemy* getAttackTarget(/* TYPE */ targetPriority)
+{
+    if (/* targetPriority == Oldest */)
+    {
+        return targetQueue.front();
+    } 
+    else if(/* targetPriority == Newest */)
+    {
+        return targetQueue.back();
+    } 
+    
+    static thread_local std::mt19937 rng(std::random_device{}()); // create a random number generator
+    std::uniform_int_distribution<std::size_t> dist(0, alive.size() - 1); // create a uniform distribution for the size of the list of alive enemies
+    attackTarget = alive[dist(rng)]; // set the attack target to a random enemy in the list of alive enemies
+
+}
+
+bool rangeCheck(Enemy* enemy /* absolute position */)
+{
+    const sf::Vector2f d = enemy.getPosition() - capPos;
+    if (d.x * d.x + d.y * d.y <= capRangeSq) 
+    {
+        return true;
+    }
+    else 
+    {
+        return false
+    } // pythagorean distance calculation to check if the enemy is in range of the capsule
+}
+
+bool checkIndex(int spawnIndex) 
+{
+    // If the spawn index of this enemy is less than or equal to the spawn index that Tail Node points to, return false. Else return true.
+    if (spawnIndex <= targetQueue.back()) 
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void addEnemy(int spawnIndex)
+{
+    targetQueue.push_back(spawnIndex);
+}
+
+void removeEnemy(int spawnIndex, /* TYPE */ targetPriority)
+{
+    
+}
+
+void refreshTargetQueue()
+{
+    // for all enemy in enemies, call rangeCheck, call checkIndex(), call isAlive(). All three must be true to enter the loop.
+    // Inside the loop, call addEnemy    
+    for (int i = 0; i < enemies.size(); i++)
+    {
+        if (enemies[i].isAlive())
+        {
+            if (rangeCheck(enemies[i]) && checkIndex(i+1))
+            {
+                addEnemy(i+1);
+            }
+        }
+    }
+}
+
+
